a2131717c6fd1777a6ad5ed0d6d02d5f3c03daae
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-21 00:15:10 +0800

    Updates

---
+++ precache-manifest.d9d2746c256d8822f45e357b30a12e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3726a538a0c8b044ab3b19bb6f69acdf",
+    "url": "/react-dad-jokes/index.html"
+  },
+  {
+    "revision": "8fd25fb0b093a706582a",
+    "url": "/react-dad-jokes/static/css/main.3ceb373f.chunk.css"
+  },
+  {
+    "revision": "3cfcbf60e5b83464004c",
+    "url": "/react-dad-jokes/static/js/2.7b5fcc9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dad-jokes/static/js/2.7b5fcc9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd25fb0b093a706582a",
+    "url": "/react-dad-jokes/static/js/main.50b44d18.chunk.js"
+  },
+  {
+    "revision": "524a489a215b6d0d9b9d",
+    "url": "/react-dad-jokes/static/js/runtime-main.cb0bd310.js"
+  }
+]);
